154efec05659c1ad6bee09e121281632c51b4e29
JeraldReb <jerald.reb@gmail.com>
2026-04-20 01:43:10 -0400

    p2 - modified create fleet to also create all rows

--- alien_fleet.py
+++ alien_fleet.py
@@ -22,29 +22,44 @@ class AlienFleet:
     def create_fleet(self):
         """Creates the alien fleet"""
         alien_h = self.settings.alien_h
+        alien_w = self.settings.alien_w
         screen_h = self.settings.screen_h
+        screen_w = self.settings.screen_w
 
-        fleet_h = self.calculate_fleet_size(alien_h, screen_h)
+        fleet_h, fleet_w = self.calculate_fleet_size(alien_h, screen_h, alien_w, screen_w)
+        
+        half_screen = self.settings.screen_w//2
         fleet_vertical_space = fleet_h * alien_h
+        fleet_horizontal_space = fleet_w * alien_h
         y_offset = int((screen_h - fleet_vertical_space)//2)
+        x_offset = int((half_screen - fleet_horizontal_space)//2)
 
-        for row in range(fleet_h):
-            current_y = alien_h * row + y_offset
-            if row % 2 ==0:
-                continue
-            self._create_alien(current_y, 10)
+        for col in range(fleet_w):
+            for row in range(fleet_h):
+                current_y = alien_h * row + y_offset
+                current_x = alien_w * col + x_offset
+                if row % 2 == 0 or col % 2 == 0:
+                    continue
+                self._create_alien(current_y, current_x)
 
-    def calculate_fleet_size(self, alien_h, screen_h):
+    def calculate_fleet_size(self, alien_h, screen_h, alien_w, screen_w):
         """Calculates the possible size of the alien fleet"""
         fleet_h = (screen_h//alien_h)
+        fleet_w = ((screen_w / 2)//alien_w)
 
-        if fleet_h % 2 ==0:
+        if fleet_h % 2 == 0:
             fleet_h -= 1
 
         else: 
             fleet_h -=2
 
-        return fleet_h
+        if fleet_w % 2 == 0:
+            fleet_w -= 1
+
+        else:
+            fleet_w -= 2
+
+        return int(fleet_h), int(fleet_w)
 
     def _create_alien(self, current_y: int, current_x: int):
         """Creates aliens within the fleet"""
