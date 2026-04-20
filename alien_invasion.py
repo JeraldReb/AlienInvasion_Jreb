7ab9f69a335d152827222468060b778e08afaf28
JeraldReb <jerald.reb@gmail.com>
2026-04-19 20:33:01 -0400

    Initialized new repository because I apparently created the last one wrong and GitHub kept throwing errors at me

---
+++ alien_invasion.py
@@ -0,0 +1,94 @@
+"""Lab 13/Alien Invasion part 2
+Jerald Reb
+Continuing to create Alien Invasion game
+Starter code from Unit 12 and 13 resources
+April 19, 2026"""
+
+import sys
+import pygame
+from settings import Settings
+from ship import Ship
+from arsenal import Arsenal
+
+class AlienInvasion:
+    """Creates a class to house the game"""
+    
+    def __init__(self):
+        """Initializes the game"""
+        pygame.init()
+        self.settings = Settings()
+
+        self.screen = pygame.display.set_mode((self.settings.screen_w,self.settings.screen_h))
+        pygame.display.set_caption(self.settings.name)
+
+        self.bg = pygame.image.load(self.settings.bg_file)
+        self.bg = pygame.transform.scale(self.bg, (self.settings.screen_w, self.settings.screen_h))
+
+        self.running = True
+        self.clock = pygame.time.Clock()
+
+        pygame.mixer.init()
+        self.laser_sound = pygame.mixer.Sound(self.settings.laser_sound)
+        self.laser_sound.set_volume(0.5)
+
+        self.ship = Ship(self, Arsenal(self))
+
+    def run_game(self):
+        """Begins to run the game"""
+        # Game Loop
+        while self.running:
+            self._check_events()
+            self.ship.update()
+            self._update_screen()
+            self.clock.tick(self.settings.FPS)
+
+    def _update_screen(self):
+        """Updates the screen and objects on it"""
+        self.screen.blit(self.bg, (0,0))
+        self.ship.draw()
+        pygame.display.flip()
+
+    def _check_events(self):
+        """Checks for user input and responds to it"""
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                self.running = False
+                pygame.quit()
+                sys.exit()
+
+            elif event.type == pygame.KEYDOWN:
+                self._check_keydown_events(event)
+
+            elif event.type == pygame.KEYUP:
+                self._check_keyup_events(event)
+
+    def _check_keydown_events(self, event):
+        """Checks for key presses and responds to them"""
+        if event.key == pygame.K_UP:
+            self.ship.moving_up = True
+        
+        elif event.key == pygame.K_DOWN:
+            self.ship.moving_down = True
+
+        elif event.key == pygame.K_SPACE:
+            if self.ship.fire():
+                # play the laser sound
+                self.laser_sound.play()
+                self.laser_sound.fadeout(250)
+
+        elif event.key == pygame.K_q:
+            self.running = False
+            pygame.quit()
+            sys.exit()
+                
+    def _check_keyup_events(self, event):
+        """Checks for key releases and responds to them"""
+        if event.key == pygame.K_UP:
+            self.ship.moving_up = False
+        
+        elif event.key == pygame.K_DOWN:
+            self.ship.moving_down = False
+
+if __name__ == '__main__':
+    ai = AlienInvasion()
+    ai.run_game()
---
+++ ship.py
@@ -0,0 +1,53 @@
+import pygame
+from settings import Settings
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from alien_invasion import AlienInvasion
+    from arsenal import Arsenal
+
+class Ship:
+    """Creates a class to house the ship"""
+
+    def __init__(self, game: 'AlienInvasion', arsenal: 'Arsenal'):
+        """Creates a function to initialize the ship"""
+        self.game = game
+        self.settings = game.settings
+        self.screen = game.screen
+        self.boundaries = self.screen.get_rect()
+
+        self.image = pygame.image.load(self.settings.ship_file)
+        self.image = pygame.transform.scale(self.image, (self.settings.ship_w, self.settings.ship_h))
+        self.image = pygame.transform.rotate(self.image, (90))
+
+        self.rect = self.image.get_rect()
+        self.rect.midright = self.boundaries.midright
+        self.moving_up = False
+        self.moving_down = False
+        self.y = float(self.rect.y)
+        self.arsenal = arsenal
+
+    def update(self):
+        """Updates the ship position on the screen"""
+        self._update_ship_movement()
+        self.arsenal.update_arsenal()
+
+    def _update_ship_movement(self):
+        temp_speed = self.settings.ship_speed
+        
+        if self.moving_up and self.rect.top > self.boundaries.top:
+            self.y -= temp_speed
+        
+        if self.moving_down and self.rect.bottom < self.boundaries.bottom:
+            self.y += temp_speed
+
+        self.rect.y = self.y
+
+    def draw(self):
+        """Draws the ship on the screen"""
+        self.arsenal.draw()
+        self.screen.blit(self.image, self.rect)
+
+    def fire(self):
+        """Fires the bullets"""
+        return self.arsenal.fire_bullet()
